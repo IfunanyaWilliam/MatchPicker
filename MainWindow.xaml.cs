693ed841882080d7949fd0294dd089d763baf556
IfunanyaWilliam <williamifunanya@gmail.com>
2022-04-21 17:54:12 +0100

    Implement GameInit() method and call in the constructor of MainWindow class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,32 @@ namespace MatchPicker
         public MainWindow()
         {
             InitializeComponent();
+            GameInit();
+        }
+
+        private void GameInit()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐙", "🐙",
+                "🐘", "🐘",
+                "🐝", "🐝",
+                "🐶", "🐶",
+                "🦘", "🦘",
+                "🐜", "🐜",
+                "🐢", "🐢",
+                "🐫", "🐫"
+            };
+
+            Random random = new Random();
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+
         }
     }
 }
