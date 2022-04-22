ddd8e6f12dd1f75e0d36d56a2f6be5b13d8238c1
IfunanyaWilliam <williamifunanya@gmail.com>
2022-04-22 15:57:23 +0100

    Add time functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Threading;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,30 +21,63 @@ namespace MatchPicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int timeElapsed;
+        int matchesFound;
         TextBlock lastTextBlockClicked;
         bool findingMatch = false;
 
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             GameInit();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timeElapsed++;
+            TimerTextBlock.Text = (timeElapsed / 10F).ToString("0.0s");
+            if(matchesFound == 8)
+            {
+                timer.Stop();
+                TimerTextBlock.Text = TimerTextBlock.Text + " Game is over, Play again";
+            }
+        }
+
+        private void TimerTextBlock_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                GameInit();
+            }
         }
 
         private void GameInit()
         {
             List<string> animalEmoji = new List<string>()
             {
-                "🐙", "🐙", "🐘", "🐘", "🐝", "🐝",  "🐶", "🐶", "🦘", "🦘", "🐜", "🐜", "🐢", "🐢", "🐫", "🐫"
+                "🐙", "🐘", "🐘", "🐝", "🐙", "🐝",  "🐶", "🐶", "🦘", "🐜", "🐜", "🐢", "🐢", "🐫", "🐫", "🦘"
             };
 
             Random random = new Random();
             foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                if(textBlock.Name != "TimerTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
             }
+
+            //Start timer after displaying animal Emojis in grid
+            timer.Start();
+            timeElapsed = 0;
+            matchesFound = 0;
 
         }
 
@@ -58,6 +92,7 @@ namespace MatchPicker
             }
             else if(textBlock.Text == lastTextBlockClicked.Text)
             {
+                matchesFound++;
                 textBlock.Visibility = Visibility.Hidden;
                 findingMatch = false;
             }
